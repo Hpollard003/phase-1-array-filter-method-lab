c95c480fe2a504a22ccbaedac80e5bce98b5287b
Hpollard003 <hpollard003@gmail.com>
2021-03-31 17:47:38 -0400

    commit tree 790650965e805c4c2dfda223cd9891899b0a8cb7

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, match) {
+    let filteredArray = array.filter(name => name.toLowerCase() === match.toLowerCase())
+    return filteredArray
+}
+//const names = drivers.filter(findMatching)
+
+function fuzzyMatch(arr,string){
+    const newArray = arr.filter(function(element){
+        return element.charAt(0) === string.charAt(0);
+    });
+    return newArray;
+}
+
+function matchName(drivers, names ) {
+    let home = drivers.filter(place => place.name === names)
+    return home
+
+}
